cd70dfb17760a519b989ae9582a8159a4b098c1d
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-03 15:44:22 -0500

    Backend project app-notes

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const notesRoutes = require("./routes/notes.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(notesRoutes);
+
+app.use((err, req, res, next) => {
+	return res.json({ message: err.message });
+});
+
+app.listen(3000);
+console.log("Server started on port 3000");
---
+++ src/routes/notes.routes.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const pool = require("../db");
+const router = Router();
+const {
+	getAllNotes,
+	getNote,
+	createNote,
+	deleteNote,
+	updateNote,
+} = require("../controllers/notes.controller");
+
+router.get("/notes", getAllNotes);
+
+router.get("/notes/:id", getNote);
+
+router.post("/notes", createNote);
+
+router.delete("/notes/:id", deleteNote);
+
+router.put("/notes/:id", updateNote);
+
+module.exports = router;

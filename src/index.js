ef27ee8d08b86bb39a5c9a631a1744630b0a9814
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-27 21:27:25 -0500

    ♻️  Refactor: use environment variable for server port configuration

--- src/index.js
+++ src/index.js
@@ -16,5 +16,7 @@ app.use((err, req, res, next) => {
 	return res.json({ message: err.message });
 });
 
-app.listen(3000);
-console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);

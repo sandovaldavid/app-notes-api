7224568c5c9edb26a98efeeb3ff97b407888286d
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-27 21:59:38 -0500

    🐛 Fix: ensure notes table is created only if it does not exist

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,5 @@
-CREATE DATABASE db_webnotes
-
-CREATE TABLE notes (
+CREATE TABLE IF NOT EXISTS notes (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
+    content TEXT NOT NULL
 );

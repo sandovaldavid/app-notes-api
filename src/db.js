23cdfa4a4ecf55b7b6b82f871dc502147b796f70
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-28 02:32:56 -0500

    ✨ Update: refactor database configuration to use connection string and simplify Pool initialization

--- src/db.js
+++ src/db.js
@@ -1,12 +1,6 @@
 const { Pool } = require("pg");
-const {db} = require("./config");
+const { db } = require("./config");
 
-const pool = new Pool({
-	user: db.user,
-	password: db.password,
-	host: db.host,
-	port: db.port,
-	database: db.database,
-});
+const pool = new Pool(db);
 
 module.exports = pool;
